bd4c40956b61faa9c54e2a05d283d095d787d1d6
Shriya Pattapu <spattapu@ucsd.edu>
2025-07-04 19:09:53 -0700

    initial layout done

--- portfolio-website/src/App.jsx
+++ portfolio-website/src/App.jsx
@@ -3,9 +3,15 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <h1>Hello, React!</h1> 
+      <h1> Hello, React! </h1> 
     </div>
   );
 }
-
+function NavBar() {
+  return (
+    <div>
+      <h1> Hello, React! </h1> 
+    </div>
+  );
+}
 export default App;
--- portfolio-website/src/main.jsx
+++ portfolio-website/src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // Use ReactDOM from react-dom/client for React 18+
-import App from './App'; // Import App component
-import './index.css'; // Optional: If you have a CSS file for styling
+import ReactDOM from 'react-dom/client'; // React 18+
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
